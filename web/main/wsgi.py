df5ff8f1f3b1de02a33f54eb9ff4e48deca8e5b1
Chandirasekar T <chandru9279+npm@gmail.com>
2015-03-08 10:42:42 +0530

    images, and wsgi settings

--- web/main/wsgi.py
+++ web/main/wsgi.py
@@ -1,14 +1,12 @@
 """
-WSGI config for zasz project.
+WSGI config for zasz.me.linux project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
-For more information on this file, see
-https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "zasz.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "main.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
